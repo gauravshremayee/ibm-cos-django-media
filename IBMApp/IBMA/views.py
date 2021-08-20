b09df2944df0f00417df1846cedcdf81e58440eb
Gaurav <gaurav.2897@gmail.com>
2021-08-20 11:07:03 +0530

    Update views.py

--- IBMApp/IBMA/views.py
+++ IBMApp/IBMA/views.py
@@ -5,8 +5,8 @@ from ibm_botocore.client import Config
 
 cos = ibm_boto3.client(
     service_name='s3',
-    ibm_api_key_id='P3NumbDp2YClbtOhbolYnDmMUmHP-fpTcz10iBoYoK9K',
-    ibm_service_instance_id="crn:v1:bluemix:public:cloud-object-storage:global:a/ffbc32d94c3a4f16bcd2db52110a859a:21e49f99-4053-4adb-af7c-0073f8222c9a::",
+    ibm_api_key_id=os.getenv("MY_COS_IBM_KEY_TEMP"),
+    ibm_service_instance_id=os.getenv("MY_SVC_IBM_INST_ID_TEMP"),
     config=Config(signature_version='oauth'),
     endpoint_url='https://s3.us.cloud-object-storage.appdomain.cloud'
 )
